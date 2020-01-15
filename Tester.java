a45b6ea2f69ceed9f63ee9f5b2e41ce8a0127cdb
nans1899 <anandabakti18@gmail.com>
2020-01-15 00:33:25 -0800

    penambahan Tester.java

---
+++ Tester.java
@@ -0,0 +1,11 @@
+
+public class Tester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("heloo");
+    }
+    
+}
